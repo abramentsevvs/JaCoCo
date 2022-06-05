f4a923da4f8ee1abbf7984238b85e99f88e21816
abramentsevvs <abramentsevvs@gmail.com>
2022-06-05 15:39:41 +0300

    new test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaximum() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1000, 1500, 3000, 5000, 10000, 151515, 27980, 1255555, 25252567, 75600, 999999};
+        long expected = 25252567;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
